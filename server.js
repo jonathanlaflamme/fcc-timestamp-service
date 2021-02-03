8198b202877deac7b4d4057f51a946e4d8262a20
Jonathan Laflamme <jonathan.laflamme.jl@gmail.com>
2021-02-03 14:34:24 -0500

    commit working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,25 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/:date?', (req, res) => {
+  let { date: dateParam } = req.params;
 
+  let date;
+  if (!dateParam) {
+    date = new Date();
+  } else if (isNaN(dateParam)) {
+    date = new Date(dateParam);
+  } else {
+    date = new Date(parseInt(dateParam));
+  }
+
+  return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
